79beb96909b3fb6bf9e41627ae5b96b64be932e7
David Stewart <davidstewy@gmail.com>
2018-08-07 17:16:29 -0400

    trying my first component

--- todos/src/App.js
+++ todos/src/App.js
@@ -3,18 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 import todoList from './todos.json';
 
+const TodoItem = props => (
+  
+  
+  <li className='completed'>
+     <div className='view'>
+        <input className='toggle' type='checkbox' checked/>
+        <label>Taste JavaScript</label>
+        <button className='destroy'></button>
+     </div>
+  </li>
+  // if completed insert a classname, otherwise dont
+);
+
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <section className='todoapp'>
+      <header className='header'>
+        <h1>todos</h1>
+        <input className='new-todo' placeholder='What needs to be done?' autofocus/>
+      </header>
+      <section className='main'>
+        <ul className='todo-list'>
+          <li className='completed'>
+            <div className='view'>
+              <input className='toggle' type='checkbox' checked/>
+              <label>Taste JavaScript</label>
+              <button className='destroy'></button>
+            </div>
+          </li>
+          <li>
+            <div className='view'>
+              <input className='toggle' type='checkbox'/>
+              <label>Buy a unicorn</label>
+              <button className='destroy'></button>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <footer className='footer'>
+        <span className='todo-count'><strong>0</strong> item(s) left</span>
+        <button className='clear-completed'>Clear completed</button>
+      </footer>
+    </section>
     );
   }
 }
